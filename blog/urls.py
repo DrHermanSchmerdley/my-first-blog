c64aaf6f892bbf37daa0bcae6c020d6d2949ece8
DrHermanSchmerdley <allseeingallknowing@hotmail.com>
2021-04-20 11:27:33 +0100

    linked to model data

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
